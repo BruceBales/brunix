c360d18afe466d3a2cadf10671a53b6f5d2bd853
Bruce <bruce.morgan.bales@gmail.com>
2020-12-20 12:44:19 -0800

    Fix ISO build script

--- buildiso.sh
+++ buildiso.sh
@@ -4,8 +4,8 @@
 
 cd initramfs-busybox
 
-find . -print0 | cpio --null -ov --format=newc | gzip -9 > ../initrd.img
+find . -print0 | cpio --null -ov --format=newc | gzip -9 > ../CD_ROOT/boot/initrd.img
 
 cd ..
 
-mkisofs -o iso/brunix.iso -b boot/isolinux/isolinux.bin -c boot/isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table CD_ROOT
+mkisofs -o iso/brunix-v01.iso -b boot/isolinux/isolinux.bin -c boot/isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table CD_ROOT
